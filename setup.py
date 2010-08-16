40898b440045456a3d5b52ccd210934648f5e1d2
Tim Fletcher <fletcher@liftinteractive.com>
2010-08-16 17:01:56 -0600

    Initial commit

---
+++ setup.py
@@ -0,0 +1,21 @@
+from distutils.core import setup
+
+setup(
+    name = "loft",
+    version='0.4',
+    author = "Ludwik Trammer and Tim Fletcher"
+    author_email = "ludwik@gmail.com, fletcher@liftinteractive.com",
+    description = "A fork of dimasbka's django-tagging-autocomplete app",
+    license = "GNU",
+    url = "http://github.com/l1f7/django-tagging-autocomplete",
+    packages = [
+        "django-tagging-autocomplete"
+    ],
+    classifiers = [
+        "Environment :: Web Environment",
+        "Intended Audience :: Developers",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        "Framework :: Django",
+    ]
+)

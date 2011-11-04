f13e21e4a34a3b6d95cad57a10474e3996dc1d71
Jordan Reiter <jordanreiter@gmail.com>
2011-11-04 12:03:18 -0300

    Going back to using json

--- tagging_autocomplete/views.py
+++ tagging_autocomplete/views.py
@@ -1,9 +1,8 @@
 from django.http import HttpResponse
-from django.core import serializers
 from django.utils import simplejson as json
 
 from tagging.models import Tag
 
 def list_tags(request):
-        tags = serializers.serialize('json', Tag.objects.filter(name__istartswith=request.GET['q']), fields=('name','id'))
+        tags = json.dumps(list(Tag.objects.filter(name__istartswith=request.GET['q']).values('name').distinct()))
 	return HttpResponse(tags, mimetype='application/json')

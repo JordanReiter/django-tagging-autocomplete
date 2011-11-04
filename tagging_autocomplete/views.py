6e42be61535f0979c45c9b0be66e22c4d7734463
Jordan Reiter <jordanreiter@gmail.com>
2011-11-04 11:51:37 -0300

    right.

--- tagging_autocomplete/views.py
+++ tagging_autocomplete/views.py
@@ -6,7 +6,7 @@ from tagging.models import Tag
 
 def list_tags(request):
 	try:
-		tags = [t.name for t in Tag.objects.filter(name__istartswith=request.GET['q']).values('name').distinct()]
+		tags = [t['name'] for t in Tag.objects.filter(name__istartswith=request.GET['q']).values('name').distinct()]
 	except MultiValueDictKeyError:
 		tags = []
 	
